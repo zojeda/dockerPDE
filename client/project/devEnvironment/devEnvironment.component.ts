fe3e53a0392bf0427fef5335556ec71bade4a96d
Zacarías F. Ojeda <zojeda@hexacta.com>
2016-03-22 14:01:48 -0300

    working on layouts

--- client/project/devEnvironment/devEnvironment.component.ts
+++ client/project/devEnvironment/devEnvironment.component.ts
@@ -6,13 +6,24 @@ class DevEnvironment { };
 let devEnvironment: angular.IComponentOptions = {
 	controller: DevEnvironment,
 	template: `
-			<md-button aria-label="menu" class="md-fab md-warn">
-				<h2><i class="fa fa-laptop"></i></h2>
-			</md-button>
+				<md-sidenav
+					class="md-sidenav-left"
+					md-component-id="left"
+					md-is-locked-open="$mdMedia('gt-md')"
+					md-disable-backdrop
+					md-whiteframe="1">
+				<md-content layout-padding layout="column" style="width: 50px;">
 
-			<md-button ng-repeat="(command, details) in dev.environment.commands" aria-label="Command" class="devCommandButton md-fab md-raised md-mini">
-				<i class="fa fa-laptop"></i>
-			</md-button>
+					<md-button aria-label="menu" class="md-fab md-warn" layout-align="center center">
+						<h2><i class="fa fa-laptop"></i></h2>
+					</md-button>
+		
+					<md-button ng-repeat="(command, details) in dev.environment.commands" aria-label="Command" class="devCommandButton md-fab md-raised md-mini">
+						<i class="fa fa-laptop"></i>
+					</md-button>
+				</md-content>
+			</md-sidenav>
+
 	`,
 	controllerAs: "dev",
 	bindings: {
